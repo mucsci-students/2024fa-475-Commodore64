60b13470dc7669fae22ce65a2b2883fbcafd94f6
TommyN17 <tommyn0017@gmail.com>
2024-10-05 00:07:53 -0400

    Cutscene update

--- ProcGenDungeon/Assets/Scripts/SceneTrigger.cs
+++ ProcGenDungeon/Assets/Scripts/SceneTrigger.cs
@@ -6,7 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneTrigger : MonoBehaviour
 {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     void OnTriggerEnter2D (Collider2D col) {
-        SceneManager.LoadScene(2);
+        StartCoroutine(LoadLevel(2));
+    }
+
+    IEnumerator LoadLevel (int levelIndex) {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
