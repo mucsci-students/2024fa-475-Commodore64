0b4b00111f610f0d089f59aa9c21066f2909f672
Francesco Spagnolo <francescospag24@gmail.com>
2024-10-11 15:41:22 -0400

    Pause menu and funtionality
    adding pause menu and ability to resume, restart, and quit game

--- ProcGenDungeon/Assets/Scripts/PauseGame.cs
+++ ProcGenDungeon/Assets/Scripts/PauseGame.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
     public void Pause()
     {
         if (Time.timeScale == 1)
         {
             Time.timeScale = 0;
+            PauseMenuUI.SetActive(true);
         }
         else
         {
             Time.timeScale = 1;
+            PauseMenuUI.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        PauseMenuUI.SetActive(false);
+        SceneManager.LoadScene("StartingRoom");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
         }
     }
 }
